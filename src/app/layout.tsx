fedd05b38d7d31996d1e6eecfa4b314d5526641a
micky00x1-beep <micky00x1@gmail.com>
2026-08-10 17:46:15 +0200

    feat: add Google AdSense integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9859876822901716"
+          crossOrigin="anonymous"
+        />
+      </head>
+
       <body className="min-h-full flex flex-col">
         <Navbar />
 
